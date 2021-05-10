12d52b702f5227aae20ece9d525683ed020bc674
toanle312 <toanle3122002@gmail.com>
2021-05-10 17:39:53 +0700

    làm gọn

--- Login.cpp
+++ Login.cpp
@@ -1,4 +1,6 @@
 #include"LOGIN.h"
+#include"Log_as_ad.h"
+#include"Log_as_stu.h"
 //
 bool check_leap_year(int y)
 {
@@ -144,4 +146,111 @@ int count_acc_in_csv(string name)
 string account(string usr, string psw)
 {
 	return usr + ',' + psw;
+}
+//
+void check_acc_student(string usr, string psw)
+{
+	ifstream f;
+	f.open("acc_sv.csv");
+	string acc = account(usr, psw);
+	int c = 0;
+	int n = count_acc_in_csv("acc_sv");
+	while (!f.eof())
+	{
+		c++;
+		string acc1;
+		f >> acc1;
+		if (acc.compare(acc1) != 0)
+		{
+			if (c < n)
+			{
+				continue;
+			}
+			else if (c == n)
+			{
+				cout << "\t\tWrong password or username !!!" << endl;
+				cout << "\t\tPress Enter to return -Main Menu- !!!" << endl;
+				cin.ignore();
+				//Nhan phim 0 de quay ve menu chinh
+			}
+		}
+		else
+		{
+			cout << "\t\tLogin sucessed !!!" << endl;
+			break;
+		}
+	}
+	f.close();
+}
+void login_as_student(string& user, string& pass)
+{
+	int d = 0, m = 0, y = 0;
+	enter_dmy(d, m, y);
+	cin.ignore();
+	if (check_dmy(d, m, y))
+	{
+		if (enter_acc(user, pass))
+		{
+			check_acc_student(user, pass);
+		}
+	}
+	else enter_dmy(d, m, y);
+}
+//
+void check_acc_ad(string usr, string psw)
+{
+	if (psw[0] == 'A' && psw[1] == 'D')
+	{
+		ifstream f;
+		f.open("acc_ad.csv");
+		string acc = account(usr, psw);
+		int c = 0;
+		int n = count_acc_in_csv("acc_ad");
+		while (!f.eof())
+		{
+			c++;
+			string acc1;
+			f >> acc1;
+			if (acc.compare(acc1) != 0)
+			{
+				if (c < n)
+				{
+					continue;
+				}
+				else if (c == n)
+				{
+					cout << "\t\tWrong password or username !!!" << endl;
+					cout << "Press Enter to return menu main !!!" << endl;
+					cin.ignore();
+					//Nhan phim 0 de quay ve menu chinh
+				}
+			}
+			else
+			{
+				cout << "\t\tLogin sucessed !!!" << endl;
+				break;
+			}
+		}
+		f.close();
+	}
+	else
+	{
+		cout << "\t\tWrong password or username !!!" << endl;
+		cout << "\t\tPress Enter to return -Main Menu- !!!" << endl;
+		cin.ignore();
+	}
+}
+void login_as_admin(string& user, string& pass)
+{
+	int d = 0, m = 0, y = 0;
+	enter_dmy(d, m, y);
+	cin.ignore();
+	if (check_dmy(d, m, y))
+	{
+		if (enter_acc(user, pass))
+		{
+			check_acc_ad(user, pass);
+		}
+	}
+	else enter_dmy(d, m, y);
 }
