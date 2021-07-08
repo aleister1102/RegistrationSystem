3b80fb1a94e005658b3666f76986dc24546ae35e
toanle312 <toanle3122002@gmail.com>
2021-07-08 19:15:18 +0700

    Update

--- Update DKHP/Header/Class.h
+++
@@ -1,7 +0,0 @@
-#include "MainSource.h"
-
-string Class_Init();
-int Class_Display(string year_path);
-string Class_Select(string year_name,int &line_number);
-string Faculty_Name(int depart,int system);
-bool Class_Proc(string year_name,int option);
--- Update DKHP/Header/Date.h
+++
@@ -1,6 +0,0 @@
-#include "Header\MainSource.h"
-void output_dmy(date dmy);
-bool check_leap_year(int year);
-int day_of_month(int month, int year);
-bool check_dmy(date dmy);
-void enter_dmy(date& dmy);
